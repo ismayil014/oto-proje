dad04fd1c32d108f6b190ca06e3baacfd915a2aa
ismayil014 <032280123@ogr.uludag.edu.tr>
2023-01-01 21:15:21 +0300

    oko

---
+++ nmj.py
@@ -0,0 +1,7 @@
+echo "# oto-proje" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:ismayil014/oto-proje.git
+git push -u origin main
