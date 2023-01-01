b0d50807e76fdabd95bacdaaadc7b8a12f6ba90f
ismayil014 <032280123@ogr.uludag.edu.tr>
2023-01-01 17:47:18 +0300

    gelistirildi

---
+++ basket projesi.py
@@ -0,0 +1,12 @@
+import math
+import pygame
+import os
+from math import atan, pi
+
+DEBUG_MODE = False
+
+PLAY_SPEED = 10
+MOUSE_DIST_D = 5
+WIDTH = 1200   # Width : 6.75m
+WIDTH_IN_METER = 6.75
+HEIGHT = 800
