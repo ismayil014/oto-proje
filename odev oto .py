12ae29eede8ea454e4c223c1fb1950f847c79927
ismayil014 <032280123@ogr.uludag.edu.tr>
2023-01-02 09:55:50 +0300

    ekleme

--- odev oto .py
+++ odev oto .py
@@ -7,3 +7,12 @@ DEBUG_MODE = False
 
 PLAY_SPEED = 10
 MOUSE_DIST_D = 5
+
+
+WIDTH = 1200   # Width : 6.75m
+WIDTH_IN_METER = 6.75
+WIDTH = 1200   # Width : 6.75m
+WIDTH_IN_METER = 6.75
+HEIGHT = 800
+APP_NAME = "Eğik Atış"
+FPS = 60
